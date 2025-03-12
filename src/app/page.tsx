542dd2d0f2e4d42f26c03f8719575131bb6bbbb2
LunEyx <luneyxlab@gmail.com>
2025-03-13 00:39:19 +1100

    Remove Amplify and Add Cognito login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,59 @@
-const Home = async () => {
-  return
+'use client'
+import { AuthProvider, useAuth } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_eHKU6HdFR",
+  client_id: "78asrfssbhr20i95rsjes57dhd",
+  redirect_uri: "http://localhost:3000/inventory",
+  response_type: "code",
+  scope: "email openid",
+};
+
+const Container = () => {
+  return (
+    <AuthProvider {...cognitoAuthConfig}>
+      <Home />
+    </AuthProvider>
+  )
 }
 
-export default Home
+const Home = () => {
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "78asrfssbhr20i95rsjes57dhd";
+    const logoutUri = "http://localhost:3000";
+    const cognitoDomain = "https://lucky-draw.auth.ap-southeast-2.amazoncognito.com";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+    </div>
+  );
+}
+
+export default Container
